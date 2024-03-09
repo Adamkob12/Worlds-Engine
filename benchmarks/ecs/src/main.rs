056fe2736fc2a23ef8852432d57611472091b419
Adam <46227443+Adamkob12@users.noreply.github.com>
2024-03-09 19:52:12 +0200

    adding benchmarks to compare against Bevy

--- benchmarks/ecs/src/main.rs
+++ benchmarks/ecs/src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 
 use bevy_ecs::prelude as bevy;
+use worlds_ecs::entity::EntityId;
 use worlds_ecs::prelude::*;
 
 #[derive(Component, bevy::Component)]
@@ -21,35 +22,40 @@ fn main() {
 
     println!(" \n ");
 
-    compare_spawning_entities(&mut bevy_world, &mut world);
+    compare_spawning_entities(&mut bevy_world, &mut world, 10_000_000);
     compare_querying(&mut bevy_world, &mut world);
 }
 
-fn compare_spawning_entities(bevy_world: &mut bevy::World, world: &mut World) {
+fn compare_spawning_entities(
+    bevy_world: &mut bevy::World,
+    world: &mut World,
+    amount_to_spawn: usize,
+) {
     // Spawn Bench 1
     compare_code_blocks! {
-        { (0..500_000).for_each(|i| {
+        { (0..amount_to_spawn).for_each(|i| {
             bevy_world.spawn((A(i), B(i), C(i), D(i)));
         }) },
 
-         { (0..500_000).for_each(|i| {
+         { (0..amount_to_spawn).for_each(|i| {
             world.spawn((A(i), B(i), C(i), D(i)));
         })}, "Spawn bench 1"
     }
 
     // Spawn Bench 1
     compare_code_blocks! {
-        { (0..500_000).for_each(|i| {
+        { (0..amount_to_spawn).for_each(|i| {
             bevy_world.spawn((B(i), D(i)));
         }) },
 
-         { (0..500_000).for_each(|i| {
+         { (0..amount_to_spawn).for_each(|i| {
             world.spawn((B(i), D(i)));
         })}, "Spawn bench 2"
     }
 }
 
 fn compare_querying(bevy_world: &mut bevy::World, world: &mut World) {
+    println!(" \n ");
     // Query Bench 1
     compare_code_blocks! {
         {
@@ -103,22 +109,27 @@ fn compare_querying(bevy_world: &mut bevy::World, world: &mut World) {
         {
             world.query_filtered::<(EntityId, &mut B), Not<Has<A>>>().for_each(|_| {});
         },
-        "Query bench 3"
+        "Query bench 4"
     }
 }
 
 #[macro_export]
 macro_rules! compare_code_blocks {
     ($bevy:block, $worlds:block, $msg:literal) => {
+
+        println!("|  {}  |", $msg);
+
         let bevy_instant = std::time::Instant::now();
-
-        println!("\n|  {}  |", $msg);
-
         $bevy
-        println!("\t Bevy: {:?}", bevy_instant.elapsed());
+        let bevy_time = bevy_instant.elapsed();
+        println!("\t Bevy: {:?}", bevy_time);
 
         let worlds_instant = std::time::Instant::now();
         $worlds
-        println!("\t Worlds: {:?}", worlds_instant.elapsed());
+        let worlds_time = worlds_instant.elapsed();
+        println!("\t Worlds: {:?}", worlds_time);
+        println!("  RATIO: {} (worlds / bevy)  ", worlds_time.as_nanos() / bevy_time.as_nanos());
+
+        println!("  {}  ", "-".repeat($msg.len()));
     };
 }
