16596db9c04f943d461ac570a2709346d023cdc8
Adam <46227443+Adamkob12@users.noreply.github.com>
2024-03-09 20:27:44 +0200

    added benchmarks against bevy_ecs 0.1

--- benchmarks/ecs/src/main.rs
+++ benchmarks/ecs/src/main.rs
@@ -1,33 +1,37 @@
 #![allow(unused)]
 
-use bevy_ecs::prelude as bevy;
+use bevy_ecs_1::prelude as bevy1;
+use bevy_ecs_13 as bevy_ecs;
+use bevy_ecs_13::prelude as bevy13;
 use worlds_ecs::entity::EntityId;
 use worlds_ecs::prelude::*;
 
-#[derive(Component, bevy::Component)]
+#[derive(Component, bevy13::Component)]
 struct A(usize);
 
-#[derive(Component, bevy::Component)]
+#[derive(Component, bevy13::Component)]
 struct B(usize);
 
-#[derive(Component, bevy::Component)]
+#[derive(Component, bevy13::Component)]
 struct C(usize);
 
-#[derive(Component, bevy::Component)]
+#[derive(Component, bevy13::Component)]
 struct D(usize);
 
 fn main() {
-    let mut bevy_world = bevy::World::default();
+    let mut bevy_world = bevy13::World::default();
     let mut world = World::default();
+    let mut bevy1_world = bevy1::World::default();
 
     println!(" \n ");
 
-    compare_spawning_entities(&mut bevy_world, &mut world, 10_000_000);
-    compare_querying(&mut bevy_world, &mut world);
+    compare_spawning_entities(&mut bevy_world, &mut bevy1_world, &mut world, 200_000);
+    compare_querying(&mut bevy_world, &mut bevy1_world, &mut world);
 }
 
 fn compare_spawning_entities(
-    bevy_world: &mut bevy::World,
+    bevy_world: &mut bevy13::World,
+    bevy1_world: &mut bevy1::World,
     world: &mut World,
     amount_to_spawn: usize,
 ) {
@@ -36,10 +40,14 @@ fn compare_spawning_entities(
         { (0..amount_to_spawn).for_each(|i| {
             bevy_world.spawn((A(i), B(i), C(i), D(i)));
         }) },
-
+        { (0..amount_to_spawn).for_each(|i| {
+            bevy1_world.spawn((A(i), B(i), C(i), D(i)));
+        })},
          { (0..amount_to_spawn).for_each(|i| {
             world.spawn((A(i), B(i), C(i), D(i)));
-        })}, "Spawn bench 1"
+        })},
+
+         "Spawn bench 1"
     }
 
     // Spawn Bench 1
@@ -47,14 +55,22 @@ fn compare_spawning_entities(
         { (0..amount_to_spawn).for_each(|i| {
             bevy_world.spawn((B(i), D(i)));
         }) },
-
+        { (0..amount_to_spawn).for_each(|i| {
+            bevy_world.spawn((B(i), D(i)));
+        }) },
          { (0..amount_to_spawn).for_each(|i| {
             world.spawn((B(i), D(i)));
-        })}, "Spawn bench 2"
+        })},
+
+         "Spawn bench 2"
     }
 }
 
-fn compare_querying(bevy_world: &mut bevy::World, world: &mut World) {
+fn compare_querying(
+    bevy_world: &mut bevy13::World,
+    bevy1_world: &mut bevy1::World,
+    world: &mut World,
+) {
     println!(" \n ");
     // Query Bench 1
     compare_code_blocks! {
@@ -65,8 +81,15 @@ fn compare_querying(bevy_world: &mut bevy::World, world: &mut World) {
                 .for_each(|_| {});
         },
         {
+            bevy1_world
+                .query::<&A>()
+                .iter()
+                .for_each(|_| {});
+        },
+        {
             world.query::<&A>().for_each(|_| {});
         },
+
         "Query bench 1"
     }
 
@@ -76,6 +99,12 @@ fn compare_querying(bevy_world: &mut bevy::World, world: &mut World) {
             bevy_world
                 .query::<(&A, &mut B)>()
                 .iter(bevy_world)
+                .for_each(|_| {});
+        },
+        {
+            bevy1_world
+                .query::<(&A, &mut B)>()
+                .iter()
                 .for_each(|_| {});
         },
         {
@@ -93,6 +122,12 @@ fn compare_querying(bevy_world: &mut bevy::World, world: &mut World) {
                 .for_each(|_| {});
         },
         {
+            bevy1_world
+                .query::<(&A, &mut B, &C, Option<&D>)>()
+                .iter()
+                .for_each(|_| {});
+        },
+        {
             world.query::<(&A, &mut B, &C, Option<&D>)>().for_each(|_| {});
         },
         "Query bench 3"
@@ -102,7 +137,7 @@ fn compare_querying(bevy_world: &mut bevy::World, world: &mut World) {
     compare_code_blocks! {
         {
             bevy_world
-                .query_filtered::<(bevy::Entity, &mut B), bevy::Without<A>>()
+                .query_filtered::<(bevy13::Entity, &mut B), bevy13::Without<A>>()
                 .iter(bevy_world)
                 .for_each(|_| {});
         },
@@ -122,14 +157,38 @@ macro_rules! compare_code_blocks {
         let bevy_instant = std::time::Instant::now();
         $bevy
         let bevy_time = bevy_instant.elapsed();
-        println!("\t Bevy: {:?}", bevy_time);
+        println!("\t Bevy ECS 0.13 \t: {:?}", bevy_time);
 
         let worlds_instant = std::time::Instant::now();
         $worlds
         let worlds_time = worlds_instant.elapsed();
-        println!("\t Worlds: {:?}", worlds_time);
-        println!("  RATIO: {} (worlds / bevy)  ", worlds_time.as_nanos() / bevy_time.as_nanos());
+        println!("\t Worlds ECS  \t: {:?}", worlds_time);
 
+        println!("  RATIO: {} (worlds / bevy)  ", worlds_time.as_nanos() / bevy_time.as_nanos());
         println!("  {}  ", "-".repeat($msg.len()));
     };
+
+    ($bevy:block, $bevy1:block, $worlds:block, $msg:literal) => {
+
+        println!("|  {}  |", $msg);
+
+        let bevy_instant = std::time::Instant::now();
+        $bevy
+        let bevy_time = bevy_instant.elapsed();
+        println!("\t Bevy ECS 0.13 \t: {:?}", bevy_time);
+
+        let bevy1_instant = std::time::Instant::now();
+        $bevy1
+        let bevy1_time = bevy_instant.elapsed();
+        println!("\t Bevy ECS 0.1 \t: {:?}", bevy1_time);
+
+        let worlds_instant = std::time::Instant::now();
+        $worlds
+        let worlds_time = worlds_instant.elapsed();
+        println!("\t Worlds ECS  \t: {:?}", worlds_time);
+
+        println!("  RATIO: {} (worlds / bevy)  ", worlds_time.as_secs_f64() / bevy_time.as_secs_f64());
+        println!("  RATIO: {} (worlds / bevy1)  ", worlds_time.as_secs_f64() / bevy1_time.as_secs_f64());
+        println!("  {}  ", "-".repeat($msg.len()));
+    }
 }
